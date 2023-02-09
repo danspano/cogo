0c30cd9bd52537946da8521956c898182bdcf619
Daniel Spano <dansonline@gmail.com>
2023-02-09 18:22:56 +1100

    Updated line painter

--- lib/painters/lines_painter.dart
+++ lib/painters/lines_painter.dart
@@ -25,6 +25,8 @@ class LinesPainter extends CustomPainter {
       ..strokeWidth = 2
       ..style = PaintingStyle.stroke;
 
+    drawBackground(canvas, size);
+
     var path = Path();
 
     var x = 0.0;
@@ -34,7 +36,7 @@ class LinesPainter extends CustomPainter {
 
     var center = Offset(size.width / 2, size.height / 2);
 
-    while (x < size.width) {
+    while (x < size.width + 10) {
       y = 0.0;
       while (y < size.height) {
         path.moveTo(x, y);
@@ -50,12 +52,13 @@ class LinesPainter extends CustomPainter {
         );
         if (coordinates != null) {
           paint.color = Color.lerp(
-            Colors.white,
-            color,
-            1 -
-                sqrt(pow(coordinates!.dx - x, 2) +
-                        pow(coordinates!.dy - y, 2)) /
-                    (size.shortestSide / 3),
+            Colors.grey.shade200,
+            getColor(coordinates ?? Offset.zero, size),
+            (1 -
+                    sqrt(pow(coordinates!.dx - x, 2) +
+                            pow(coordinates!.dy - y, 2)) /
+                        (size.shortestSide / 3))
+                .clamp(0.0, 1.0),
           )!;
         }
         canvas.drawPath(path, paint);
@@ -64,6 +67,37 @@ class LinesPainter extends CustomPainter {
       }
       x += 20;
     }
+  }
+
+  // Takes the coordinates and the canvas size as inputs
+  // and returns a Color which is chosen based on the position
+  // of the coordinates on the canvas. The canvas acts as a color
+  // picker, so as the coordinates change, the color changes. The
+  // colors cover the entire color spectrum.
+  Color getColor(Offset coordinates, Size size) {
+    var hue = coordinates.dx / size.width;
+    var saturation = coordinates.dy / size.height;
+    var value = 1.0;
+    return HSVColor.fromAHSV(1.0, hue * 360, saturation, value).toColor();
+  }
+
+  // This function takes the color from the getColor function and
+  // and uses the luminance value to pick a color lerp between
+  // black and white.
+  Color getBackgroundColor(Offset coordinates, Size size) {
+    var color = getColor(coordinates, size);
+    var luminance = color.computeLuminance();
+    return Color.lerp(color, Colors.white, luminance)!;
+  }
+
+  // This function draws the background of the canvas using the color
+  // from the getBackgroundColor function
+  void drawBackground(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = getBackgroundColor(coordinates ?? Offset.zero, size);
+    var path = Path();
+    path.addRect(Rect.fromLTWH(0, 0, size.width, size.height));
+    canvas.drawPath(path, paint);
   }
 
   @override
